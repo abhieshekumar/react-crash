cb0afa882fb8692340a8bf987f00a47ba8921024
abhieshekumar <abhieshekumar@gmail.com>
2019-03-07 00:25:22 +0530

    CSS Stylesheets - Multiple classes

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -5,7 +5,7 @@ function Stylesheet(props) {
   let className = props.primary ? 'primary':''
   return (
     <div>
-      <h1 className={className}>StyleSheets</h1>
+      <h1 className={`${className} font-xl`}>StyleSheets</h1>
     </div>
   )
 }
