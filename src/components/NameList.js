79cdd179e0c998e44ad1edb2d1128dfd889a1581
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 23:51:47 +0530

    List Rendering - Multiple Objects - Better Implementation - Separating JSX

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Person from './Person'
 
 function NameList() {
   const persons = [
@@ -21,7 +22,7 @@ function NameList() {
       skill: 'Vue.js'
     }
   ]
-  const personList = persons.map(person => <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}</h2>)
+  const personList = persons.map(person => <Person person={person} /> )
   return (
     <div>{ personList }</div>
   )
