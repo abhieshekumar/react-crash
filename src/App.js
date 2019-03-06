16a73fefbd42cd307220d1e6e93d92aaa049a65c
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 22:43:14 +0530

    Event Handling in Class Component

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Hello2 from './components/Hello2' //JSX Version of component
 import Message from './components/Message' //for state of component
 import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 class App extends Component {
   render() {
     /* If there is no content between the tag <Greet></Greet> we may change it to a self closing tag */
     return (
       <div className="App">
-        <FunctionClick/>
+        <ClassClick/>
       </div>
     );
   }
---
+++ src/components/ClassClick.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class ClassClick extends Component{
+
+  clickHandler(){
+    console.log('Class Button Clicked')
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.clickHandler}>Class Button</button>
+      </div>
+    )
+  }
+}
+
+export default ClassClick
