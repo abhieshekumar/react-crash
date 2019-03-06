555ade64e7281323637b1a43367607fa8791c27a
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 19:21:57 +0530

    Not the JSX version of Component (Rectified)

--- src/components/Hello2.js
+++ src/components/Hello2.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 const Hello2 = () => {
-  return React.createElement('div', null, '<h1>Not using JSX</h1>')
-  /* Notice the output in browser H1 is not treated as DOM Node*/
+  /*return React.createElement('div', null, '<h1>Not using JSX</h1>')
+  Notice the output in browser H1 is not treated as DOM Node*/
+  return React.createElement('div', null, React.createElement('h1',null,'Not using JSX'))
 }
 
 export default Hello2
