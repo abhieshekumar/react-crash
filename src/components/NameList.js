6559d8af8d68cd209e96542da17d91003dbcac65
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 23:57:25 +0530

    Console Error Resolved : Hey item item in list rendered using map method should have a prop named key with unique value within list

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -22,7 +22,7 @@ function NameList() {
       skill: 'Vue.js'
     }
   ]
-  const personList = persons.map(person => <Person person={person} /> )
+  const personList = persons.map(person => <Person key={person.id} person={person} /> )
   return (
     <div>{ personList }</div>
   )
