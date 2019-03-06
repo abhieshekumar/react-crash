facf80518c4c6657f96acbd690841be7c40759e9
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 18:48:42 +0530

    Functional Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'  /* You may omit the .js extension */
 
 class App extends Component {
   render() {
+    /* If there is no content between the tag <Greet></Greet> we may change it to a self closing tag */
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Greet/>
       </div>
     );
   }
