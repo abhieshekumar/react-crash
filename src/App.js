bec81150d62348b96fb35a40076a5770aac90ece
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 19:15:06 +0530

    JSX version of Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Greet from './components/Greet'  /* You may omit the .js extension */
 import { Greeting } from './components/GreetNamed' // for the named functional conponent
 import Welcome from './components/Welcome'  //Class Component
+import Hello from './components/Hello' //JSX Version of component
 
 class App extends Component {
   render() {
