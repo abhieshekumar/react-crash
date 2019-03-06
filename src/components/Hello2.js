168f695b57c6c493de15a327e4a4f6cacceb0971
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 19:25:33 +0530

    Not the JSX version of Component (Rectified Class Name)

--- src/components/Hello2.js
+++ src/components/Hello2.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Hello2 = () => {
   /*return React.createElement('div', null, '<h1>Not using JSX</h1>')
-  Notice the output in browser H1 is not treated as DOM Node*/
-  return React.createElement('div', null, React.createElement('h1',null,'Not using JSX'))
+  
+  Notice the output in browser H1 is not treated as DOM Node
+  
+  return React.createElement('div', null, React.createElement('h1',null,'Not using JSX'))*/
+
+  /*The Second Parameter is key-value pair applied to the object */
+  return React.createElement('div',{id: 'hello', className:'SomeClass'}, React.createElement('h1',null,'Not using JSX'))
+
+  /*The console displays an error as 'class' is a reserved JS word so change it to className */
 }
 
 export default Hello2
