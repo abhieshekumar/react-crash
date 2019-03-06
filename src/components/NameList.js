aaddbfdddfcb4b3251813237d037d269f406c595
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 23:41:15 +0530

    List Rendering

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -4,9 +4,9 @@ function NameList() {
   const names = ['Bruce', 'Clark', 'Diana']
   return (
     <div>
-      <h2>{names[0]}</h2>
-      <h2>{names[1]}</h2>
-      <h2>{names[2]}</h2>
+      {
+        names.map( name => <h2>{name}</h2> )
+      }
     </div>
   )
 }
