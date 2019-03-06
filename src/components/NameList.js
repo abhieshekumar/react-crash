c00596a6aba52f0e8507f9129a09d7897f24cbd9
abhieshekumar <abhieshekumar@gmail.com>
2019-03-06 23:46:16 +0530

    List Rendering - Multiple Objects

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
 function NameList() {
-  const names = ['Bruce', 'Clark', 'Diana']
-  const nameList = names.map(name => <h2>{name}</h2>)
+  const persons = [
+    {
+      id: 1,
+      name: 'Bruce',
+      age: 30,
+      skill: 'React'
+    },
+    {
+      id: 2,
+      name: 'Bruce',
+      age: 25,
+      skill: 'Angular'
+    },
+    {
+      id: 3,
+      name: 'Diana',
+      age: 28,
+      skill: 'Vue.js'
+    }
+  ]
+  const personList = persons.map(person => <h2>I am {person.name}. I am {person.age} years old. I know {person.skill}</h2>)
   return (
-    <div>{ nameList }</div>
+    <div>{ personList }</div>
   )
 }
 
