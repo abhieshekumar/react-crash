b0f95f8c50395d588e92b1f3ce0e7e8f3dc0fb2e
abhieshekumar <abhieshekumar@gmail.com>
2019-03-07 13:31:59 +0530

    Fragment Demo4 - Shorthand of Fragments

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -11,17 +11,9 @@ export default function Columns() {
     }
   ]
   return (
-    <React.Fragment>
-      {
-        items.map(item => (
-          <React.Fragment key={item.id}>
-            <h1>Title</h1>
-            <p>{item.title}</p>
-          </React.Fragment>
-        ))
-      }
+    <>
       <td>Name</td>
       <td>Jane Doe</td>
-    </React.Fragment>
+    </>
   )
 }
