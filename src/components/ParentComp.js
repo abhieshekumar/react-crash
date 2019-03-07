f28afe43d6c306c33c9da5fc26b864d629f10ab0
abhieshekumar <abhieshekumar@gmail.com>
2019-03-07 14:24:57 +0530

    Understanding Message Logged in Log - Why not Pure Component in console logged

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react'
 import RegularComponent from './RegularComponent';
 import PureComp from './PureComp';
 
+/*The Regular Component does not implement the shouldComponentUpdate method. It always return true by default*/
+
+/*
+A Pure Component implements shouldComponentUpdate with shallow props and state comparison
+*/
+
+/*
+Shallow Comparison
+  Primitive Types
+    a SC b is TRUE if a and b have same value and are of dame type
+  
+  Complex Types
+    a SC b returns TRUE if a and b reference the same object
+
+    var a = [1,2,3]
+    var b = [2]
+    var c = a
+
+    a SC b is FALSE
+    a SC c is TRUE
+*/
+
 class ParentComp extends Component {
 
   constructor(props) {
