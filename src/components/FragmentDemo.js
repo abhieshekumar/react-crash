def656a8b848f2b289606864fd9e2cf682f458c4
abhieshekumar <abhieshekumar@gmail.com>
2019-03-07 13:22:16 +0530

    Fragment Demo1

--- src/components/FragmentDemo.js
+++ src/components/FragmentDemo.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function FragmentDemo() {
   return (
-    <div>
-      Fragment Demo
-    </div>
+    <React.Fragment>
+    {/*Allows to return multiple elements from render which other wise are wrapped in a single parent component*/}
+      <h1>
+        Fragment Demo
+      </h1>
+      <p>
+        This is a description of Demo
+      </p>
+    </React.Fragment> 
   )
 }
 
